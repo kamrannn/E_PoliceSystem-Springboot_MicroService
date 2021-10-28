4e25bed309af1b1f13b60a97f563b1c16a8ce54b
kamranabbasi <rajakamran737@gmail.com>
2021-10-28 14:58:22 +0500

    second commit

---
+++ src/main/java/com/app/epolice/model/entity/PoliceStation.java
@@ -0,0 +1,85 @@
+package com.app.epolice.model.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "t_police_station")
+public class PoliceStation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String city;
+    private String address;
+    private String createdDate;
+    private String updatedDate;
+    private boolean active;
+
+    public PoliceStation() {
+    }
+
+    public PoliceStation(String name, String city, String address, String createdDate, String updatedDate, boolean active) {
+        this.name = name;
+        this.city = city;
+        this.address = address;
+        this.createdDate = createdDate;
+        this.updatedDate = updatedDate;
+        this.active = active;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(String createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getUpdatedDate() {
+        return updatedDate;
+    }
+
+    public void setUpdatedDate(String updatedDate) {
+        this.updatedDate = updatedDate;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+}
---
+++ src/main/java/com/app/epolice/util/EmailNotification.java
@@ -0,0 +1,32 @@
+package com.app.epolice.util;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailNotification {
+
+    private JavaMailSender javaMailSender;
+    public EmailNotification(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    public EmailNotification() {
+    }
+
+    /**
+     * This method is using accepting email ID and message to send mail through JavaMailSender(SMTP)
+     * @Author "Kamran"
+     * @CreatedDate "14-10-2021
+     * @param email
+     * @param message
+     */
+    public void sendMail(String email, String message) {
+        SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setTo(email, email);
+        msg.setSubject("Account Verification Token");
+        msg.setText(message);
+        javaMailSender.send(msg);
+    }
+}
