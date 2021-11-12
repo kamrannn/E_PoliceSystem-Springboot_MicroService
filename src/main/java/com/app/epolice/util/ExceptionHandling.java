69ebb8798bf1fd68d170256dc27c285e9a51defa
kamranabbasi <rajakamran737@gmail.com>
2021-11-12 09:44:13 +0500

    validation has been applied

--- src/main/java/com/app/epolice/util/ExceptionHandling.java
+++ src/main/java/com/app/epolice/util/ExceptionHandling.java
@@ -19,23 +19,6 @@ import javax.validation.ConstraintViolationException;
  */
 @ControllerAdvice
 public class ExceptionHandling {
-    /**
-     * Handle validation exceptions map.
-     *
-     * @param ex the ex
-     * @return the map
-     */
-/*
-    public static ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getAllErrors().forEach((error) -> {
-            String fieldName = ((FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
-        });
-        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
-    }
-*/
 
     /**
      * Input validation exception response entity.
